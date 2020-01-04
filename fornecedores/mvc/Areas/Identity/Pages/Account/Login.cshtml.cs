85794b3f8499096b95bbb8c1d19d3cea85d65e0e
Rafael Guinho <rafaelguinho@outlook.com>
2020-01-04 07:38:01 -0300

    Tradução das views do identity

--- fornecedores/mvc/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ fornecedores/mvc/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -45,13 +45,15 @@ namespace mvc.Areas.Identity.Pages.Account
         {
             [Required]
             [EmailAddress]
+            [Display(Name = "E-mail")]
             public string Email { get; set; }
 
             [Required]
             [DataType(DataType.Password)]
+            [Display(Name = "Senha")]
             public string Password { get; set; }
 
-            [Display(Name = "Remember me?")]
+            [Display(Name = "Lembrar?")]
             public bool RememberMe { get; set; }
         }
 
